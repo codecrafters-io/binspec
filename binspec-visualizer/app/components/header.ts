a96598bb2894981d97c1fab8478f3ddefe82f832
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-09-24 17:26:11 +0100

    Fix header component name and add Glint type declarations for registry.

--- binspec-visualizer/app/components/header.ts
+++ binspec-visualizer/app/components/header.ts
@@ -1,3 +1,9 @@
 import Component from '@glimmer/component';
 
 export default class Header extends Component {}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    Header: typeof Header;
+  }
+}
--- binspec-visualizer/types/global.d.ts
+++ binspec-visualizer/types/global.d.ts
@@ -1,1 +1,7 @@
 import '@glint/environment-ember-loose';
+
+import type PageTitle from 'ember-page-title/template-registry';
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry extends PageTitle {}
+}
