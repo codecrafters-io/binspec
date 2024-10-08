e49194fa18267c00a5a166c779eb867910f04d79
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-10-08 18:19:28 +0100

    update

---
+++ binspec-visualizer/app/components/format-documentation.ts
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+import type { DataSegment } from 'binspec-visualizer/lib/data-segment';
+import type HoverStateService from 'binspec-visualizer/services/hover-state';
+
+type Signature = {
+  Args: {
+    data: Uint8Array;
+    segments: DataSegment[];
+    highlightedSegment?: DataSegment;
+    onSegmentSelect: (segment: DataSegment) => void;
+    onSegmentMouseEnter: (
+      section: NonNullable<HoverStateService['initiatedFromSection']>,
+      segment: DataSegment,
+      byteIndex?: number,
+      event?: MouseEvent,
+    ) => void;
+    onSegmentMouseLeave: () => void;
+  };
+};
+
+export default class FormatDocumentation extends Component<Signature> {}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    FormatDocumentation: typeof FormatDocumentation;
+  }
+}
